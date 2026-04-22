05fdf4a24cfdbcc2ad477c39bb57aeed0453f0c6
Andrea <apqu>
2026-04-22 15:16:36 +0200

    commit before pull

--- main/file_io.py
+++ main/file_io.py
@@ -7,8 +7,8 @@ and managing file paths and pickled data.
 Author  :   Vyacheslav Li (until 2.0), Andrea Pupic, Alexei Gurchenko (later versions)
 Email   :   vyacheslav.li.1991@gmail.com, andrea.pupic@ist.ac.at, alexei.gurchenko@ist.ac.at
 Date    :   07.30.2024 (2.0)
-Update  :   11.2025 
-Version :   2.3.3
+Update  :   04.2026 
+Version :   3.0
 Contact :   https://hostenlab.pages.ist.ac.at/contact/
 '''
 
